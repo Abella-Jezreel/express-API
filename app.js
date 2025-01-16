0fa58b8e670e7e3092d8b05795cc8488bc4a2dd0
Abella-Jezreel <teamabella@outlook.com>
2025-01-16 17:20:28 +0800

    creating routes and controllers

--- app.js
+++ app.js
@@ -6,13 +6,6 @@ const feedRoutes = require('./routes/feed');
 const app = express();
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-    res.on('finish', () => {
-        console.log(res.getHeaders());
-    });
-    next();
-});
-
 // we need to set headers to avoid CORS issues
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');

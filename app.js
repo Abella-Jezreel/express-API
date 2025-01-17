7137dc9d60a1bdc4dd92abe4b388ae520b18f63e
Abella-Jezreel <teamabella@outlook.com>
2025-01-17 12:02:34 +0800

    backend code

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ app.get('/', (req, res) => {
     res.send('Welcome to the Express API');
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(3001, () => {
+    console.log('Server is running on port 3001');
     }
 );
 

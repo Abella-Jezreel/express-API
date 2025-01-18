cd7bbe89f7e39efdd3579ff4ea4bb37bf794378a
Abella-Jezreel <teamabella@outlook.com>
2025-01-18 15:26:09 +0800

    back end code

--- controllers/feed.js
+++ controllers/feed.js
@@ -2,36 +2,62 @@ const { validationResult } = require("express-validator");
 
 const Post = require("../models/post");
 
-exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: "1",
-        title: "First Post",
-        content: "This is the first post!",
-        imageUrl: "images/duck.jpg", // This is a dummy URL
-        creator: {
-          name: "John Doe",
-        },
-        createdAt: new Date(),
-      },
-    ],
-  });
+// exports.getPosts = (req, res, next) => {
+//   res.status(200).json({
+//     posts: [
+//       {
+//         _id: "1",
+//         title: "First Post",
+//         content: "This is the first post!",
+//         imageUrl: "images/duck.jpg",
+//         creator: {
+//           name: "John Doe",
+//         },
+//         createdAt: new Date(),
+//       },
+//     ],
+//   });
+// };
+
+exports.getPosts = async (req, res, next) => {
+  try {
+    const posts = await Post.find();
+    res.status(200).json({
+      posts: posts,
+    });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
 
-// exports.getPosts = async (req, res, next) => {
-//   try {
-//     const posts = await Post.find();
-//     res.status(200).json({
-//       posts: posts,
-//     });
-//   } catch (err) {
-//     if (!err.statusCode) {
-//       err.statusCode = 500;
-//     }
-//     next(err);
-//   }
-// };
+// New getPost function
+exports.getPost = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Invalid post ID.');
+    error.statusCode = 422;
+    return next(error);
+  }
+ 
+  const postId = req.params.postId;
+  try {
+    const post = await Post.findById(postId);
+    if (!post) {
+      const error = new Error('Could not find post.');
+      error.statusCode = 404;
+      throw error;
+    }
+    res.status(200).json({ post: post });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
 
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
--- routes/feed.js
+++ routes/feed.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { body } = require('express-validator');
+const { body, param  } = require('express-validator');
 
 const feedController = require('../controllers/feed');
 
 const router = express.Router();
 
-router.get('/post', feedController.getPosts); 
+router.get('/posts', feedController.getPosts); 
+router.get('/post/:postId', param('postId').isMongoId(), feedController.getPost);
 router.post('/post', [
     body('title').trim().isLength({ min: 5 }),
     body('content').trim().isLength({ min: 5 }),

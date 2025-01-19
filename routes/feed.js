6c578070c01568d181726871e4daa1f1ae42f6e0
Abella-Jezreel <teamabella@outlook.com>
2025-01-19 16:39:53 +0800

    backend code

--- routes/feed.js
+++ routes/feed.js
@@ -10,6 +10,14 @@ router.get('/post/:postId', param('postId').isMongoId(), feedController.getPost)
 router.post('/post', [
     body('title').trim().isLength({ min: 5 }),
     body('content').trim().isLength({ min: 5 }),
-], feedController.createPost);  
+], feedController.createPost);
+
+router.put('/post/:postId', [
+    param('postId').isMongoId(),
+    body('title').trim().isLength({ min: 5 }),
+    body('content').trim().isLength({ min: 5 }),
+], feedController.updatePost);
+
+router.delete('/post/:postId', param('postId').isMongoId(), feedController.deletePost);
 
 module.exports = router;

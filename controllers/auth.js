4e18a382b3c6e627c92cbdb2432d246fb67d8b34
Abella-Jezreel <teamabella@outlook.com>
2025-01-20 17:37:24 +0800

    backend code

--- controllers/auth.js
+++ controllers/auth.js
@@ -93,4 +93,29 @@ exports.getStatus = (req, res, next) => {
     });
 };
 
+exports.updateStatus = (req, res, next) => {
+  const newStatus = req.body.status;
+  User.findById(req.userId)
+    .then((user) => {
+      if (!user) {
+        const error = new Error("User not found.");
+        error.statusCode = 404;
+        throw error;
+      }
+      user.status = newStatus;
+      return user.save();
+    })
+    .then((result) => {
+      res.status(200).json({ message: "User updated." });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+
+
 
--- routes/auth.js
+++ routes/auth.js
@@ -28,3 +28,5 @@ router.put('/signup', [
 ], authController.signup);
 
 router.post('/login', authController.login);
+
+module.exports = router;
